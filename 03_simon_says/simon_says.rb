77e95e31b2e96b7f232a77c90c569465ef5dc186
Duy DANG <tao.zen.duy@gmail.com>
2017-10-10 17:16:55 +0200

    Simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
 #write your code here
+def echo(string)
+  string
+end
+
+def shout(string)
+  string.upcase
+end
+
+def repeat(string, times = 2)
+  string + (" " + string)*(times -1)
+end
+
+def start_of_word(string,num)
+  string[0,num]
+end
+
+def first_word(string)
+  string[0,string.index(" ")]
+end
+
+def titleize(string)
+  dictionnary = ["the","at","from","over","and"]
+  array_str = string.split(" ")
+  array_str.each do |word|
+    word.capitalize! unless !dictionnary.index(word).nil?
+  end
+  array_str[0].capitalize!
+  string = array_str.join(" ")
+end
+
