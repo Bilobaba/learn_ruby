8a3914fbfa6e07d255dbd073876e05516156dae5
Duy DANG <tao.zen.duy@gmail.com>
2017-10-10 16:14:44 +0200

    Operators

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,34 @@
 #write your code here
+def add(a,b)
+  return a + b
+end
+
+def subtract(a,b)
+  return a - b
+end
+
+def sum(tab)
+  tab.sum
+end
+
+def multiply(numbers)
+  result = 1
+  numbers.each do |n|
+    result = result * n
+  end
+  return result
+end
+
+def power(a,b)
+  a**b
+end
+
+def factorial(n)
+  result = 1
+    if n>1
+    for i in (1..n)
+      result *= i
+    end
+  end
+  return result
+end
